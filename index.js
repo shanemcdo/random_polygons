f7f1e0832edc04fcb6e8b9ea84c78d563b8fbfaf
Shane McDonough <shanemcdman@gmail.com>
2022-01-14 19:11:09 -0800

    Allow ability to graph based off midpoints

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const els = {
     'num_points': document.querySelector('#num-points'),
 }
+let history = [];
 
 function windowResized(){
     resizeCanvas(windowWidth, windowHeight);
@@ -8,24 +9,39 @@ function windowResized(){
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
-    stroke(255);
-    noFill();
     els.num_points.addEventListener('input', ()=>{
         let val = parseInt(els.num_points.value || '0');
         if(val > els.num_points.max)
             els.num_points.value = els.num_points.max;
         else if(val < els.num_points.min)
             els.num_points.value = els.num_points.min;
-        
-        redraw();
+        reset();
     });
+    reset();
+    stroke(255);
+    noFill();
     noLoop();
+}
+
+function keyPressed(){
+    if(keyCode === 32 || keyCode === RIGHT_ARROW){
+        go_forward();
+    }else if(keyCode === LEFT_ARROW){
+        go_back();
+    }
 }
 
 function draw(){
     translate(windowWidth / 2, windowHeight / 2);
     background(0);
-    draw_polygon(make_random_polygon());
+    draw_polygon(polygon);
+    let prev = previous();
+    if(prev !== null){
+        push();
+        stroke('red');
+        draw_polygon(prev);
+        pop();
+    }
 }
 
 function random_vector(){
@@ -43,11 +59,53 @@ function make_random_polygon(){
     return result;
 }
 
+function make_next_polygon(points){
+    let result = [];
+    let prev = points[points.length - 1];
+    for(let pos of points){
+        result.push(midpoint(pos, prev));
+        prev = pos
+    }
+    return result;
+}
+
 function draw_polygon(points){
     beginShape();
     for(let pos of points){
         vertex(pos.x, pos.y);
-        circle(pos.x, pos.y, 10);
+        push();
+        strokeWeight(10);
+        point(pos.x, pos.y)
+        pop();
     }
     endShape(CLOSE);
+}
+
+function midpoint(a, b){
+    return a.copy().add(b).div(2)
+}
+
+function previous(){
+    if(history.length === 0)
+        return null;
+    return history[history.length - 1];
+}
+
+function go_forward(){
+    history.push(polygon);
+    polygon = make_next_polygon(polygon);
+    redraw();
+}
+
+function go_back(){
+    if(previous() !== null){
+        polygon = history.pop();
+        redraw();
+    }
+}
+
+function reset(){
+    polygon = make_random_polygon();
+    history = [];
+    redraw();
 }
