2662227c4ec06df768c5e08e5b5fd6a34d6b321c
Shane McDonough <shanemcdman@gmail.com>
2022-01-14 17:37:10 -0800

    Add code for drawing random polygons

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-function main(){
-    console.log('Hello World!');
+function windowResized(){
+    resizeCanvas(windowWidth, windowHeight);
 }
 
-main();
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    stroke(255);
+    noFill();
+    noLoop();
+}
+
+function draw(){
+    translate(windowWidth / 2, windowHeight / 2);
+    background(0);
+    draw_polygon(make_random_polygon());
+}
+
+function random_vector(){
+    return createVector(
+        random(-windowWidth / 2, windowWidth / 2),
+        random(-windowHeight / 2, windowHeight / 2)
+    );
+}
+
+function make_random_polygon(num_sides = 10){
+    let result = [];
+    for(let i = 0; i < num_sides; i++)
+        result.push(random_vector());
+    return result;
+}
+
+function draw_polygon(points){
+    beginShape();
+    for(let pos of points){
+        vertex(pos.x, pos.y);
+        circle(pos.x, pos.y, 10);
+    }
+    endShape(CLOSE);
+}
